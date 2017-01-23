869b12605732a261dea31e4b9d900c3be8a9502c
silesky <seth.silesky@gmail.com>
2017-01-23 12:46:26 -0600

    basic animation

--- AutosuggestExample/AutoSuggest.js
+++ AutosuggestExample/AutoSuggest.js
@@ -1,19 +1,21 @@
-'use strict';
-import {debounce} from 'throttle-debounce';
 import React, {
   Component
 } from 'React';
-import {   
+import {
+  Animated,
   StyleSheet,
   Text,
   TextInput,
   ListView,
   TouchableOpacity,
-  View, 
-} from 'react-native';
+  View,
 
+} from 'react-native';
+import { debounce } from 'throttle-debounce';
+
+const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 const termsArr = [
-  "Birthday", 
+  "Birthday",
   "Graduation",
   "Anniversary",
   "Homecoming",
@@ -24,30 +26,35 @@ const termsArr = [
   "Secret Santa",
   "Chanukah"
 ];
-
-
-const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-export default class AutoSuggestInput extends Component {
+export default class AutoSuggest extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      results: termsArr,
+      results: [],
       currentInput: null,
-
+      opacity: new Animated.Value(0),
+      
     };
+
   }
-  setCurrentInput(currentInput) { this.setState({currentInput}) }
-  clearTerms() { this.setState({results: []}) }
-  addAllTerms() { this.setState({results: termsArr}) }
+  componentDidMount() {
+    Animated.timing(this.state.opacity, {
+      toValue:  1, 
+      duration: 2000,
+    }).start();
+  }
+  setCurrentInput(currentInput) { this.setState({ currentInput }) }
+  clearTerms() { this.setState({ results: [] }) }
+  addAllTerms() { this.setState({ results: termsArr }) }
   searchTerms(currentInput) {
-    this.setState({currentInput});
+    this.setState({ currentInput });
     debounce(200, () => {
       const results = termsArr.filter((eachTerm => eachTerm.toLowerCase().indexOf(currentInput.toLowerCase()) > -1))
-      this.setState({results})
+      this.setState({ results })
     })()
   }
   renderSeperator() {
-      return (<View
+    return (<View
         style={{
           flex: 1,
           height:  4,
@@ -56,41 +63,43 @@ export default class AutoSuggestInput extends Component {
       />)
   }
   render() {
-
+    console.log('opacity', this.state.opacity._value)
     return (
       <View style={styles.container}>
-        <TextInput 
-            spellCheck={false}
-            defaultValue={this.state.currentInput}
-            onBlur={() => console.log('blur') /* this.clearTerms() */}
-            onFocus={() => this.addAllTerms()}
-            onChangeText={(el) => this.searchTerms(el)}
-            placeholder="Gift"
-            style={styles.text_input}
-             />
-        <ListView
-          initialListSize={15}
-          enableEmptySections
-          renderSeperator={this.renderSeperator}
-          dataSource={ds.cloneWithRows(this.state.results)}
-          renderRow={(rowData, sectionId, rowId, highlightRow) => 
-            <TouchableOpacity 
-              activeOpacity={0.5}
-              style={styles.container}
-              onPress={(el) => {
-                this.setCurrentInput('hello');
-                this.setCurrentInput(this.state.results[rowId])
-              }}>
-              <View style={{opacity: 0.4, paddingBottom: 5, paddingTop: 5, borderColor: 'lightgrey', borderBottomWidth: 1, borderRightWidth: 1, borderLeftWidth: 1}}>
-                <Text style={{fontSize: 18, lineHeight: 30}}>{rowData}</Text>
-
-                </View>
-            </TouchableOpacity>}
-         />
-         </View>
+          <TextInput
+              spellCheck={false}
+              defaultValue={this.state.currentInput}
+              onBlur={() => console.log('blur') /* this.clearTerms() */}
+              onFocus={() => this.addAllTerms()}
+              onChangeText={(el) => this.searchTerms(el)}
+              placeholder="Gift"
+              style={styles.text_input}
+              />
+          <View>
+            <ListView
+              initialListSize={15}
+              enableEmptySections
+              renderSeperator={this.renderSeperator}
+              dataSource={ds.cloneWithRows(this.state.results)}
+              renderRow={(rowData, sectionId, rowId, highlightRow) =>
+                <TouchableOpacity
+                  activeOpacity={0.5 /* when you touch it the text color grimaces */}
+                  style={styles.container}
+                  onPress={(el) => {
+                    this.setCurrentInput('hello');
+                    this.setCurrentInput(this.state.results[rowId])
+                  }}>
+                  <Animated.View style={{opacity: this.state.opacity, paddingBottom: 5, paddingTop: 5, borderColor: 'lightgrey', borderBottomWidth: 1, borderRightWidth: 1, borderLeftWidth: 1}}>
+                    <Text style={{fontSize: 18, lineHeight: 30}}>{rowData}</Text>
+                    </Animated.View>
+                </TouchableOpacity> }
+              />
+        </View>
+    </View>
+         
     );
-  
-}
+
+  }
 }
 
 var styles = StyleSheet.create({
